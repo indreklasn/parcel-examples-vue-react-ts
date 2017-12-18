47a1ae8e2ecb5e42e0b6b088efd5475d46c40abc
Indrek Lasn <lasnindrek@gmail.com>
2017-12-19 00:42:40 +0100

    add ts

--- typescript/index.js
+++
@@ -1,8 +0,0 @@
-const framework = document.getElementById('tsapp')
-
-window.addEventListener('load', () => {
-  framework.style.color = '#ff3e96';
-  framework.style.fontSize = '2rem';
-  framework.style.fontWeight = 'bold';
-  framework.innerHTML = 'Hello, TypeScript!';
-})
---
+++ typescript/index.ts
@@ -0,0 +1,8 @@
+const framework = document.getElementById('tsapp')
+
+window.addEventListener('load', () => {
+  framework.style.color = '#ff3e96';
+  framework.style.fontSize = '2rem';
+  framework.style.fontWeight = 'bold';
+  framework.innerHTML = 'Hello, TypeScript!';
+})
