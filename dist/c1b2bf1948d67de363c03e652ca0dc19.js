8eb9ac066c4ad8c74e6f2fd0a692482d1fd2cfe7
Indrek Lasn <lasnindrek@gmail.com>
2017-12-18 01:34:39 +0100

    move react styles to react folder

--- dist/c1b2bf1948d67de363c03e652ca0dc19.js
+++ dist/c1b2bf1948d67de363c03e652ca0dc19.js
@@ -65,7 +65,7 @@ require = (function (modules, cache, entry) {
 
     // Override the current require with this new one
     return newRequire;
-})({15:[function(require,module,exports) {
+})({13:[function(require,module,exports) {
 var global = (1,eval)("this");
 /*!
  * Vue.js v2.5.11
@@ -7242,7 +7242,7 @@ Vue$3.nextTick(function () {
 /*  */
 
 module.exports = Vue$3;
-},{}],16:[function(require,module,exports) {
+},{}],14:[function(require,module,exports) {
 var Vue // late bind
 var version
 var map = (window.__VUE_HOT_MAP__ = Object.create(null))
@@ -7472,7 +7472,7 @@ exports.reload = tryWrap(function (id, options) {
   })
 })
 
-},{}],18:[function(require,module,exports) {
+},{}],20:[function(require,module,exports) {
 var inserted = exports.cache = {}
 
 function noop () {}
@@ -7497,7 +7497,7 @@ exports.insert = function (css) {
   }
 }
 
-},{}],5:[function(require,module,exports) {
+},{}],8:[function(require,module,exports) {
 "use strict";
 
 var __vueify_style_dispose__ = require("vueify/lib/insert-css").insert("/* line 16, stdin */\nh1[data-v-6f48ed6a] {\n  color: #05ede1; }");(function () {
@@ -7536,7 +7536,7 @@ if (module.hot) {
     }
   })();
 }
-},{"vue":15,"vue-hot-reload-api":16,"vueify/lib/insert-css":18}],17:[function(require,module,exports) {
+},{"vue":13,"vue-hot-reload-api":14,"vueify/lib/insert-css":20}],19:[function(require,module,exports) {
 var global = (1,eval)("this");
 /*!
  * Vue.js v2.5.11
@@ -18298,7 +18298,7 @@ return Vue$3;
 
 })));
 
-},{}],3:[function(require,module,exports) {
+},{}],5:[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -18329,7 +18329,7 @@ if (module.hot) {
 }
 
 exports.default = app;
-},{"./app.vue":5,"vue/dist/vue.js":17}],0:[function(require,module,exports) {
+},{"./app.vue":8,"vue/dist/vue.js":19}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
 function Module(config) {
@@ -18348,7 +18348,7 @@ function Module(config) {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent) {
-    var ws = new WebSocket('ws://localhost:52110/');
+    var ws = new WebSocket('ws://localhost:53142/');
     ws.onmessage = (e) => {
         var data = JSON.parse(e.data);
 
@@ -18440,4 +18440,4 @@ function hmrAccept(bundle, id) {
 
     return getParents(global.require, id).some(id => hmrAccept(global.require, id));
 }
-},{}]},{},[0,3])
+},{}]},{},[0,5])
--- react/index.js
+++ react/index.js
@@ -1,4 +1,4 @@
-import '../styles.scss';
+import './styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
